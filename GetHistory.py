af92ecd2e183d99d3eaae4ee3aef5def41399b99
Jordan Kuhn <jtkuhn@foxguardsolutions.com>
2017-08-11 15:18:55 -0400

    Adjust number of requested statuses
    Not a functional change, but more accurately prints how many status
    requests are being submitted.

--- GetHistory.py
+++ GetHistory.py
@@ -45,9 +45,10 @@ api = tweepy.API(auth)
 
 userVar = api.get_user(twitter_account_name)
 status_count = userVar.statuses_count
-status_total_divided = (status_count / count_per_api_call) + 1
+status_count //= 2.0; #[User].statuses_count is too large due to also counting retweets, etc, that don't get returned when the API is called to get statuses. This adjusts it into the right ballpark.
+status_requests = (status_count // count_per_api_call) + 1
 
-if not canRequestStatuses(status_total_divided):
+if not canRequestStatuses(status_requests):
 	print("Made too many API calls - can't currently get user statuses. Please wait a few minutes and try again.", flush=True)
 	exit()
 
@@ -62,7 +63,7 @@ writeStatusesToFile(statusFile, statuses)
 
 
 index = 0
-while index < status_total_divided:
+while index < status_requests:
 	print("Getting the next chunk of statuses... chunk #" + str(index + 2), flush=True)
 	statuses = api.user_timeline(twitter_account_name, count=count_per_api_call, max_id=lowestId)
 	lowestId = getLowestStatusId(statuses)
